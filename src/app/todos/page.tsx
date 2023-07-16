4b776b4ce7d109eaff79b71b6fb2163315b2353f
kawajiharuka <kjn.hk10001@gmail.com>
2023-07-16 19:11:48 +0900

    実装設計コメント追加

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -4,6 +4,21 @@ import { Add } from "./Add";
 import { TodoList } from "./TodoList";
 import { useHandle } from "./hook";
 
+/**
+ * todosの状態をAppで管理
+ * ✅Addボタン押下でtodolistを追加できる
+ * ・Addコンポでinput状態を管理
+ * ・addボタン押下時、inputをhandleAddに引数として渡す
+ * →Appでinputを受け取り、todosを更新
+ *
+ * ✅editボタンでliがinputになり、編集できるようになる。editボタンがsaveボタンに変わる
+ * ✅saveボタンでliに代わる
+ * ・edit中かの状態をTodoコンポで管理。edit状態に応じてUI変更
+ * ・編集するたびにtodosを更新する編集中のidとtaskを引数に渡す(todoごとで良い)
+ *
+ * ✅deleteボタンでliが消える
+ * ・Todoコンポから削除されたidをhandleDeleteの引数に渡す
+ */
 export type TodoType = {
   id: string;
   task: string;
